20f45acf489a494252b3b6bc229e0818717e277b
Bruce <irabruce10@gmail.com>
2024-09-20 13:04:37 +0200

    Creating react counting timer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import { useEffect, useState } from 'react';
 
 export default function App() {
-  const newyear = new Date();
-  console.log(newyear);
-  const [hour, setHour] = useState(6);
+  const newYear = new Date('1 jan 2025');
+  const newyearDate = new Date(newYear);
+  const cdate = new Date();
+
+  const totalSecond = Math.floor(newyearDate - cdate) / 1000;
+
+  const [hour, setHour] = useState(totalSecond);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setHour((prevHour) => (prevHour += -1));
-      console.log(hour);
-      console.log('setInterval');
     }, 1000);
 
     return () => clearInterval(interval);
   });
+
+  const days = Math.floor(hour / 3600 / 24);
+  const hours = Math.floor(hour / 3600) % 24;
+  const minutes = Math.floor((hour % 3600) / 60);
+  const seconds = Math.floor(hour) % 60;
   return (
     <div>
       <h1>Hello</h1>
+      days:{days} Hours:{hours} min: {minutes} sec: {seconds}
     </div>
   );
 }
